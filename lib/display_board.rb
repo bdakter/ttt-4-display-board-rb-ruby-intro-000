779bc6ec1b5040dca58439d0bf95ed4bb05868d4
Boruch Zidell <philomath613@gmail.com>
2019-02-22 19:18:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(game)
   puts "#{game[0]}|#{game[1]}|#{game[2]}"
-
+  puts "-----------"
   puts "#{game[3]}|#{game[4]}|#{game[5]}"
-
+  puts "-----------"
   puts "#{game[6]}|#{game[7]}|#{game[8]}"
 
 end
